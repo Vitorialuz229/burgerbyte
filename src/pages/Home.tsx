504872e9a5e82686fe486b4f3014ee1c34981d50
Vitorialuz229 <vitorialuz831@gmail.com>
2025-02-04 20:46:33 -0300

    :sparkles: feat: Implementando card de bebidas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@ import logo from "../assets/logo.png";
 import { ShoppingCart } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Hamburguer } from "../model/Hamburguer";
+import { Bebidas } from "../model/Bebidas";
 
 export function Home() {
   const [hamburgueres, setHamburgueres] = useState<Hamburguer[]>([]);
@@ -11,10 +12,22 @@ export function Home() {
     fetch("http://localhost:5000/hamburgueres")
       .then((response) => response.json())
       .then((data) => {
-        console.log(data); 
-        setHamburgueres(data || []); 
+        console.log(data);
+        setHamburgueres(data || []);
       })
       .catch((error) => console.error("Erro ao buscar os hambúrgueres", error));
+  }, []);
+
+  const [bebidas, setBebidas] = useState<Bebidas[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/bebidas")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setBebidas(data || []);
+      })
+      .catch((error) => console.error("Erro ao buscar os bebidas", error));
   }, []);
 
   return (
@@ -47,20 +60,31 @@ export function Home() {
       <main className="grid grid-cols-1 lg:grid-cols-2 gap-4 px-2 mb-8 mx-auto max-w-7xl">
         {/* Cards de Hambúrgueres */}
         {hamburgueres.map((hamburguer) => (
-          <div key={hamburguer.id} className="flex p-4 md:p-6 shadow-md rounded-lg bg-white w-full">
+          <div
+            key={hamburguer.id}
+            className="flex p-4 md:p-6 shadow-md rounded-lg bg-white w-full"
+          >
             <img
               src={hamburguer.image}
               alt={hamburguer.name}
               className="w-28 h-28 md:w-36 md:h-36 rounded-md hover:-rotate-2 duration-300"
             />
             <div className="ml-4 flex flex-col justify-between">
-              <p className="text-lg md:text-xl xl:text-2xl font-bold">{hamburguer.name}</p>
-              <p className="text-sm md:text-lg xl:text-x1 font-light text-gray-600">{hamburguer.description}</p>
+              <p className="text-lg md:text-xl xl:text-2xl font-bold">
+                {hamburguer.name}
+              </p>
+              <p className="text-sm md:text-lg xl:text-x1 font-light text-gray-600">
+                {hamburguer.description}
+              </p>
               <div className="flex justify-between items-center mt-4">
                 <span className="text-sm md:text-lg xl:text-x1 font-medium text-gray-900">
                   R$ {hamburguer.price.toFixed(2)}
                 </span>
-                <button className="bg-black px-4 py-2 md:px-6 md:py-3 rounded shadow-lg flex items-center">
+                <button
+                  className="bg-black px-4 py-2 md:px-6 md:py-3 rounded shadow-lg flex items-center"
+                  data-name={hamburguer.name}
+                  data-price={hamburguer.price}
+                >
                   <ShoppingCart size={18} className="text-white" />
                 </button>
               </div>
@@ -69,10 +93,41 @@ export function Home() {
         ))}
       </main>
 
-      <div>
-        <h2>
-          Bebidas
-        </h2>
+      <div className="mx-auto max-w-7xl px-2 my-2 flex justify-start w-full">
+        <h2 className="font-bold text-3xl">Bebidas</h2>
+      </div>
+
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 px-2 mb-8 mx-auto max-w-7xl">
+        {/* Cards de Bebidas */}
+        {bebidas.map((bebida) => (
+          <div
+            key={bebida.id}
+            className="flex p-4 md:p-6 shadow-md rounded-lg bg-white w-full"
+          >
+            <img
+              src={bebida.image}
+              alt={bebida.name}
+              className="w-28 h-28 md:w-36 md:h-36 rounded-md hover:-rotate-2 duration-300"
+            />
+            <div className="ml-4 flex flex-col justify-between w-full">
+              <p className="text-lg md:text-xl xl:text-2xl font-bold">
+                {bebida.name}
+              </p>
+              <div className="flex justify-between items-center mt-4">
+                <span className="text-sm md:text-lg xl:text-xl font-medium text-gray-900">
+                  R$ {bebida.price.toFixed(2)}
+                </span>
+                <button
+                  className="bg-black px-4 py-2 md:px-6 md:py-3 rounded shadow-lg flex items-center"
+                  data-name={bebida.name}
+                  data-price={bebida.price}
+                >
+                  <ShoppingCart size={18} className="text-white" />
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
