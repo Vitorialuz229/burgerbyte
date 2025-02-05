f02136e93234ded4d1421c75fbd78771455e563f
Vitorialuz229 <vitorialuz831@gmail.com>
2025-02-04 22:38:54 -0300

    :bug:	fix: Correção de layout de bebidas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -77,7 +77,7 @@ export function Home() {
                 {hamburguer.description}
               </p>
               <div className="flex justify-between items-center mt-4">
-                <span className="text-sm md:text-lg xl:text-x1 font-medium text-gray-900">
+                <span className="text-base md:text-lg xl:text-x1 font-semibold text-gray-900">
                   R$ {hamburguer.price.toFixed(2)}
                 </span>
                 <button
@@ -97,7 +97,7 @@ export function Home() {
         <h2 className="font-bold text-3xl">Bebidas</h2>
       </div>
 
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4 px-2 mb-8 mx-auto max-w-7xl">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 px-2 max-w-7xl">
         {/* Cards de Bebidas */}
         {bebidas.map((bebida) => (
           <div
@@ -113,8 +113,11 @@ export function Home() {
               <p className="text-lg md:text-xl xl:text-2xl font-bold">
                 {bebida.name}
               </p>
+              <p className="text-sm md:text-lg xl:text-x1 font-light text-gray-600">
+                {bebida.description}
+              </p>
               <div className="flex justify-between items-center mt-4">
-                <span className="text-sm md:text-lg xl:text-xl font-medium text-gray-900">
+                <span className="text-base md:text-lg xl:text-xl font-semibold text-gray-900">
                   R$ {bebida.price.toFixed(2)}
                 </span>
                 <button
@@ -130,15 +133,56 @@ export function Home() {
         ))}
       </div>
 
-
+      {/* Modal Card */}
+      <div
+        className="bg-black/60 w-full h-full fixed top-0 left-0 z-[99] hidden items-center justify-center"
+        id="modal"
+      >
+        <div className="bg-white p-5 rounded-md min-w-[90%] md:min-w-[600px] max-w-[500px] sm:max-w-[400px]">
+          <h2 className="text-center font-bold text-2xl sm:text-xl mb-4">
+            Meu carrinho
+          </h2>
+          <div
+            id="cart-items"
+            className="flex justify-between mb-4 flex-col"
+          ></div>
+          <p className="font-bold text-lg sm:text-base">
+            Total: <span>0.00</span>
+          </p>
+          <p className="font-bold text-lg mt-4 sm:text-base">
+            Endereço de Entrega:
+          </p>
+          <input
+            type="text"
+            className="w-full sm:w-[80%] p-1 border-2 rounded my-2 text-lg sm:text-base"
+            placeholder="Endereço de entrega"
+            id="address"
+          />
+          <p className="text-red-500 hidden" id="address-warn">
+            Digite seu endereço completo!
+          </p>
+          <div className="flex items-center justify-between mt-6">
+            <button className="text-black text-xl" id="close" type="button">
+              Fechar
+            </button>
+            <button
+              className="bg-custom-green text-white px-4 py-2 rounded-md ml-2 text-xl"
+              id="finish"
+              type="button"
+            >
+              Finalizar Pedido
+            </button>
+          </div>
+        </div>
+      </div>
 
       {/* Footer */}
       <footer className="w-full text-xl bg-red-500 py-3 fixed bottom-0 z-40 flex justify-center items-center">
-        <button className="flex text-white items-center gap-2 font-bold"
-        id="cart-button"
+        <button
+          className="flex text-white items-center gap-2 font-bold"
+          id="cart-button"
         >
-          (<span id="card-count"> 2 </span>)
-          Veja meu carrinho
+          (<span id="card-count"> 2 </span>) Veja meu carrinho
           <ShoppingCart size={25} className="text-white" />
         </button>
       </footer>
